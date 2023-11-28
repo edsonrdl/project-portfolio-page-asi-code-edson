bf7fed3cfe06f3d346ffd5df0f654994761fbf1d
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-11-28 00:22:21 -0300

    feat:create portfolio web

---
+++ README.md
@@ -0,0 +1,28 @@
+# Projeto Portfólio
+
+Este é um Projeto que criei para um web portfólio para exibir/demostrar minhas habilidades como programador de software full stack e minhas experiências e habilidades.
+
+## Visão Geral  
+
+Esse projeto tem como objetivo anexar meus projetos e habilidades conforme minha evolução como desenvolvedor e auxiliar na visualização das demais características como programador 
+
+## Tecnologias Utilizadas
+
+- **HTMLL:** 
+
+- **CSS:** 
+
+- **Javascript:** 
+
+
+## Demostração das seções
+- **Seção inicial**
+![Alt text](./assets/Readme/img-section-initial.png)
+- **Seção Portfólio**
+![Alt text](./assets/Readme/img-section-portfolio.png)
+- **Seção Sobre mim**
+![Alt text](./assets/Readme/img-section-about-me.png)
+![Alt text](./assets/Readme/img-section-about-me-text-infor.png)
+![Alt text](./assets/Readme/img-section-about-me-skills.png)
+- **Seção Contatos**
+![Alt text](./assets/Readme/img-section-contacts.png)
---
+++ js/script.js
@@ -0,0 +1,253 @@
+const sectionInitialPresentationAsiCodeActive = document.querySelector(
+  "#container-exibi-icons"
+);
+
+function activeLogoAsiCodeAnimation() {
+  const logoAsiCode = document.querySelector(".initial-logo-apresentation");
+  logoAsiCode.classList.add("logo-Active");
+}
+
+function deactivateLogoAsiCodeAnimation() {
+  const logoAsiCode = document.querySelector(".initial-logo-apresentation");
+  logoAsiCode.classList.remove("logo-Active");
+}
+
+sectionInitialPresentationAsiCodeActive.addEventListener(
+  "mouseover",
+  activeLogoAsiCodeAnimation
+);
+sectionInitialPresentationAsiCodeActive.addEventListener(
+  "mouseout",
+  deactivateLogoAsiCodeAnimation
+);
+
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("menu-nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+function scrollToSectionNav(event) {
+  event.preventDefault();
+
+  const sectionId = event.currentTarget.getAttribute("data-section");
+
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+document.querySelectorAll("[data-section]").forEach((link) => {
+  link.addEventListener("click", scrollToSectionNav);
+});
+
+let intervalId;
+let timeoutId;
+function getRandomIcon() {
+  const IconsArray = [
+    "./assets/icons/icon-.net-framework.png",
+    "./assets/icons/icon-angula.png",
+    "./assets/icons/icon-c-sharp-logo.png",
+    "./assets/icons/icon-css.png",
+    "./assets/icons/icon-docker.png",
+    "./assets/icons/icon-html-5.png",
+    "./assets/icons/icon-java.png",
+    "./assets/icons/icon-javascript.png",
+    "./assets/icons/icon-mongodb.png",
+    "./assets/icons/icon-mysql.png",
+    "./assets/icons/icon-node-js.png",
+    "./assets/icons/icon-postgres.png",
+    "./assets/icons/icon-postman.png",
+    "./assets/icons/icon-python.png",
+    "./assets/icons/icon-sass.png",
+    "./assets/icons/icon-spring-boot.png",
+    "./assets/icons/icon-sql-server.png",
+    "./assets/icons/icon-typescript.png",
+  ];
+
+  return IconsArray[Math.floor(Math.random() * IconsArray.length)];
+}
+
+function getRandomPositionDisplay() {
+  const container = document.getElementById("container-exibi-icons");
+  const widthContainer = container.clientWidth;
+  const heightContainer = container.clientHeight;
+  const width = widthContainer - 100;
+  const height = heightContainer - 100;
+  const x = Math.floor(Math.random() * width);
+  const y = Math.floor(Math.random() * height);
+  return { x, y };
+}
+
+function showIcon() {
+  const imgSrc = getRandomIcon();
+  const container = document.getElementById("container-exibi-icons");
+
+  const imgElement = document.createElement("img");
+  imgElement.className = "icon";
+  imgElement.setAttribute("src", imgSrc);
+
+  const position = getRandomPositionDisplay();
+  imgElement.style.left = position.x + "px";
+  imgElement.style.top = position.y + "px";
+  imgElement.style.visibility = "visible";
+  imgElement.style.width = "40px";
+
+  container.appendChild(imgElement);
+
+  setTimeout(() => {
+    imgElement.style.visibility = "hidden";
+  }, 5000);
+}
+
+function activateDisplayIcons() {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      clearTimeout(timeoutId);
+      showIcon();
+    }, 5000);
+  }
+}
+
+function deactivateDisplayIcons() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function checkVisibilityDisplayShowIcons() {
+  const mySectionDisplay = document.getElementById("container-exibi-icons");
+  const boundingBox = mySectionDisplay.getBoundingClientRect();
+
+  const threshold = 0.5;
+
+  const isPartiallyVisible =
+    boundingBox.bottom >= 0 &&
+    boundingBox.right >= 0 &&
+    boundingBox.top <= window.innerHeight &&
+    boundingBox.left <= window.innerWidth;
+
+  const verticalVisibility =
+    boundingBox.top <= window.innerHeight * (1 - threshold) &&
+    boundingBox.bottom >= window.innerHeight * threshold;
+
+  const horizontalVisibility =
+    boundingBox.left <= window.innerWidth * (1 - threshold) &&
+    boundingBox.right >= window.innerWidth * threshold;
+
+  if (isPartiallyVisible && verticalVisibility && horizontalVisibility) {
+    activateDisplayIcons();
+  } else {
+    deactivateDisplayIcons();
+  }
+}
+
+document
+  .getElementById("container-exibi-icons")
+  .addEventListener("mouseenter", checkVisibilityDisplayShowIcons);
+document
+  .getElementById("container-exibi-icons")
+  .addEventListener("mouseleave", checkVisibilityDisplayShowIcons);
+window.addEventListener("scroll", checkVisibilityDisplayShowIcons);
+window.addEventListener("resize", checkVisibilityDisplayShowIcons);
+window.addEventListener("load", checkVisibilityDisplayShowIcons);
+
+function downloadCv() {
+  const displayCatDownload = document.querySelector(".img-cat-display");
+
+  displayCatDownload.classList.add("img-cat-animation");
+  setTimeout(() => {
+    displayCatDownload.classList.remove("img-cat-animation");
+  }, 4000);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "../assets/cv/CV_Edson_Rodrigo_FULL_STACK_PT_BR.pdf.pdf", true);
+  xhr.responseType = "blob";
+  xhr.onload = function () {
+    if (this.status === 200) {
+      var blob = this.response;
+      saveAs(blob, "CV_Edson_Rodrigo_PT_BR");
+    }
+  };
+  xhr.send();
+}
+
+function copyContacts(text) {
+  const el = document.createElement("textarea");
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  alert("Copiado: " + text);
+}
+
+let currentIndexImgagem = 0;
+let intervalIdImg;
+
+function showNextImageCardPortfolio() {
+  const slideImgPortfolioElement =
+    document.querySelectorAll(".slide-portfolio");
+
+  const slideImg =
+    slideImgPortfolioElement[
+      Math.floor(Math.random() * slideImgPortfolioElement.length)
+    ];
+
+  const slideImgPortfolioChildren = slideImg.querySelectorAll(
+    ".slide-img-portfolio"
+  );
+  slideImgPortfolioChildren[currentIndexImgagem].style.opacity = 0;
+  currentIndexImgagem =
+    (currentIndexImgagem + 1) % slideImgPortfolioChildren.length;
+  slideImgPortfolioChildren[currentIndexImgagem].style.opacity = 1;
+
+  if (currentIndexImgagem === slideImgPortfolioChildren.length - 1) {
+    activateShowNextImageCardPortfolio();
+  }
+}
+
+function activateShowNextImageCardPortfolio() {
+  if (!intervalIdImg) {
+    intervalIdImg = setInterval(showNextImageCardPortfolio, 3000);
+  }
+}
+activateShowNextImageCardPortfolio();
+
+var modalPreview = document.getElementById("modal-portfolio-preview");
+var span = document.getElementsByClassName("close")[0];
+var btnPreview = document.querySelectorAll(".button-card-port-btn-preview-modal");
+
+btnPreview.forEach(function (button) {
+  button.addEventListener("click", showModalPreviewActive);
+});
+
+function showModalPreviewActive(event) {
+  const btnPreview = event.currentTarget;
+  const modalPreview = btnPreview.nextElementSibling;
+
+  if (modalPreview && modalPreview.classList.contains("modal-preview")) {
+    modalPreview.style.display = "block";
+  }
+}
+
+span.onclick = function () {
+  modalPreview.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modalPreview) {
+    modalPreview.style.display = "none";
+  }
+};
