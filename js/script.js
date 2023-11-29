afa0cb92d512b61ee077d85f56a8b6835c963077
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-11-28 21:32:54 -0300

    feat:Criado home

--- js/script.js
+++ js/script.js
@@ -12,37 +12,68 @@ function toggleMenu(event) {
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
   }
 }
-
-
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
 
-async function carregarJSON() {
-  try {
-    const response = await fetch('svg.json');
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Erro ao carregar o arquivo JSON:', error);
-    throw error;
-  }
+function getRandomIcon() {
+  
+  const imgsArray = [
+    "../imagens/icons/icons8-.net-framework-48.png",
+    "../imagens/icons/icons8-angular-50.png",
+    "../imagens/icons/icons8-c-sharp-logo-50.png",
+    "../imagens/icons/icons8-css-50.png",
+    "../imagens/icons/icons8-docker-50.png",
+    "../imagens/icons/icons8-html-50.png",
+    "../imagens/icons/icons8-java-50.png",
+    "../imagens/icons/icons8-javascript-50.png",
+    "../imagens/icons/icons8-microsoft-sql-server-48.png",
+    "../imagens/icons/icons8-mongodb-a-cross-platform-document-oriented-database-program-25.png",
+    "../imagens/icons/icons8-mysql-50.png",
+    "../imagens/icons/icons8-node-js-32.png",
+    "../imagens/icons/icons8-postman-api-64.png",
+    "../imagens/icons/icons8-python-50.png",
+    "../imagens/icons/icons8-spring-boot-48.png",
+    "../imagens/icons/icons8-typescript-50.png"
+  ];
+
+
+  return imgsArray[Math.floor(Math.random() * imgsArray.length)];
 }
 
-// Função para exibir os ícones na tela
-function exibirIcones(icones) {
-  const container = document.getElementById('icons-container');
-
-  icones.forEach((icone) => {
-    const div = document.createElement('div');
-    div.innerHTML = `<h3>${icone.name}</h3>${icone.path}`;
-    container.appendChild(div);
-  });
+function getRandomPosition() {
+  const container = document.getElementById('container-exibi-icons');
+  const widthContainer = container.clientWidth;
+  const heightContainer = container.clientHeight;
+  const width =widthContainer-70;
+  const height =heightContainer-70;
+  const x = Math.floor(Math.random() * width);
+  const y = Math.floor(Math.random() * height);
+  return { x, y };
 }
 
-// Carregar o JSON e exibir os ícones
-carregarJSON().then((data) => {
-  if (data && data.icons) {
-    exibirIcones(data.icons);
-  }
-});
+function showIcon() {
+  const imgSrc = getRandomIcon();
+  const container = document.getElementById('container-exibi-icons');
+
+
+  const imgElement = document.createElement('img');
+  imgElement.className = 'icon';
+  imgElement.setAttribute('src', imgSrc);
+
+
+  const position = getRandomPosition();
+  imgElement.style.left = position.x + 'px';
+  imgElement.style.top = position.y + 'px';
+  imgElement.style.visibility = 'visible';
+
+
+  container.appendChild(imgElement);
+
+
+  setTimeout(() => {
+    imgElement.style.visibility = 'hidden';
+  }, 2000);
+}
+
+setInterval(showIcon, 3000);
