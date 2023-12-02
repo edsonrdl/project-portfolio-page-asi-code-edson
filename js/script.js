c96a27212a95d07c8485082b346ea00263147c43
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-12-02 19:27:42 -0300

    feat:Finalizado section contacts

--- js/script.js
+++ js/script.js
@@ -186,3 +186,13 @@ sectionInterrogationActive.addEventListener('mouseout', toggleInterrogationAnima
   };
   xhr.send();
 };
+//Copiar contato 
+function copyContacts(text) {
+  const el = document.createElement('textarea');
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  alert('Copiado: ' + text);
+}
