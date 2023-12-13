12e7dda2a21f5e97cf4adeccc7e28f24b6ca0bf1
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-12-13 15:41:24 -0300

    refactor:Corridigo slide e modal

--- js/script.js
+++ js/script.js
@@ -1,63 +1,61 @@
-const sectionInitialPresentationAsiCodeActive = document.querySelector('#container-exibi-icons');
+const sectionInitialPresentationAsiCodeActive = document.querySelector(
+  "#container-exibi-icons"
+);
 
 function activeLogoAsiCodeAnimation() {
-  const logoAsiCode = document.querySelector('.initial-logo-apresentation');
-  logoAsiCode.classList.add('logo-Active');
+  const logoAsiCode = document.querySelector(".initial-logo-apresentation");
+  logoAsiCode.classList.add("logo-Active");
 }
 
 function deactivateLogoAsiCodeAnimation() {
-  const logoAsiCode = document.querySelector('.initial-logo-apresentation');
-  logoAsiCode.classList.remove('logo-Active');
+  const logoAsiCode = document.querySelector(".initial-logo-apresentation");
+  logoAsiCode.classList.remove("logo-Active");
 }
 
-sectionInitialPresentationAsiCodeActive.addEventListener('mouseover', activeLogoAsiCodeAnimation);
-sectionInitialPresentationAsiCodeActive.addEventListener('mouseout', deactivateLogoAsiCodeAnimation);
+sectionInitialPresentationAsiCodeActive.addEventListener(
+  "mouseover",
+  activeLogoAsiCodeAnimation
+);
+sectionInitialPresentationAsiCodeActive.addEventListener(
+  "mouseout",
+  deactivateLogoAsiCodeAnimation
+);
 
-
-
-const btnMobile = document.getElementById('btn-mobile');
+const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('menu-nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("menu-nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
   if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
   } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
 }
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
 function scrollToSectionNav(event) {
-
   event.preventDefault();
 
-
-  const sectionId = event.currentTarget.getAttribute('data-section');
-
+  const sectionId = event.currentTarget.getAttribute("data-section");
 
   const section = document.getElementById(sectionId);
   if (section) {
-    section.scrollIntoView({ behavior: 'smooth' });
+    section.scrollIntoView({ behavior: "smooth" });
   }
 }
 
-document.querySelectorAll('[data-section]').forEach(link => {
-  link.addEventListener('click', scrollToSectionNav);
+document.querySelectorAll("[data-section]").forEach((link) => {
+  link.addEventListener("click", scrollToSectionNav);
 });
 
-
-
-let intervalId; 
+let intervalId;
 let timeoutId;
 function getRandomIcon() {
-
   const IconsArray = [
     "../imagens/icons/icon-.net-framework.png",
     "../imagens/icons/icon-angula.png",
@@ -76,15 +74,14 @@ function getRandomIcon() {
     "../imagens/icons/icon-sass.png",
     "../imagens/icons/icon-spring-boot.png",
     "../imagens/icons/icon-sql-server.png",
-    "../imagens/icons/icon-typescript.png"
+    "../imagens/icons/icon-typescript.png",
   ];
 
-
   return IconsArray[Math.floor(Math.random() * IconsArray.length)];
-};
+}
 
 function getRandomPositionDisplay() {
-  const container = document.getElementById('container-exibi-icons');
+  const container = document.getElementById("container-exibi-icons");
   const widthContainer = container.clientWidth;
   const heightContainer = container.clientHeight;
   const width = widthContainer - 100;
@@ -92,28 +89,27 @@ function getRandomPositionDisplay() {
   const x = Math.floor(Math.random() * width);
   const y = Math.floor(Math.random() * height);
   return { x, y };
-};
+}
 
 function showIcon() {
   const imgSrc = getRandomIcon();
-  const container = document.getElementById('container-exibi-icons');
+  const container = document.getElementById("container-exibi-icons");
 
-  const imgElement = document.createElement('img');
-  imgElement.className = 'icon';
-  imgElement.setAttribute('src', imgSrc);
+  const imgElement = document.createElement("img");
+  imgElement.className = "icon";
+  imgElement.setAttribute("src", imgSrc);
 
   const position = getRandomPositionDisplay();
-  imgElement.style.left = position.x + 'px';
-  imgElement.style.top = position.y + 'px';
-  imgElement.style.visibility = 'visible';
+  imgElement.style.left = position.x + "px";
+  imgElement.style.top = position.y + "px";
+  imgElement.style.visibility = "visible";
 
   container.appendChild(imgElement);
 
-
   setTimeout(() => {
-    imgElement.style.visibility = 'hidden';
+    imgElement.style.visibility = "hidden";
   }, 3000);
-};
+}
 
 function activateDisplayIcons() {
   if (!intervalId) {
@@ -122,7 +118,7 @@ function activateDisplayIcons() {
       showIcon();
     }, 3000);
   }
-};
+}
 
 function deactivateDisplayIcons() {
   clearInterval(intervalId);
@@ -133,7 +129,7 @@ function checkVisibilityDisplayShowIcons() {
   const mySectionDisplay = document.getElementById("container-exibi-icons");
   const boundingBox = mySectionDisplay.getBoundingClientRect();
 
-  const threshold = 0.5; 
+  const threshold = 0.5;
 
   const isPartiallyVisible =
     boundingBox.bottom >= 0 &&
@@ -154,95 +150,86 @@ function checkVisibilityDisplayShowIcons() {
   } else {
     deactivateDisplayIcons();
   }
-};
+}
 
-document.getElementById("container-exibi-icons").addEventListener("mouseenter", checkVisibilityDisplayShowIcons);
-document.getElementById("container-exibi-icons").addEventListener("mouseleave", checkVisibilityDisplayShowIcons);
+document
+  .getElementById("container-exibi-icons")
+  .addEventListener("mouseenter", checkVisibilityDisplayShowIcons);
+document
+  .getElementById("container-exibi-icons")
+  .addEventListener("mouseleave", checkVisibilityDisplayShowIcons);
 window.addEventListener("scroll", checkVisibilityDisplayShowIcons);
 window.addEventListener("resize", checkVisibilityDisplayShowIcons);
 window.addEventListener("load", checkVisibilityDisplayShowIcons);
 
+function downloadCv() {
+  const displayCatDownload = document.querySelector(".img-cat-display");
 
- function downloadCv() {
-  const displayCatDownload = document.querySelector('.img-cat-display');
-
-  displayCatDownload.classList.add('img-cat-animation');
+  displayCatDownload.classList.add("img-cat-animation");
   setTimeout(() => {
-    displayCatDownload.classList.remove('img-cat-animation');
+    displayCatDownload.classList.remove("img-cat-animation");
   }, 4000);
 
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'CV_Edson_Rodrigo_PT_BR.pdf.pdf', true);
-  xhr.responseType = 'blob';
-  xhr.onload = function() {
-      if (this.status === 200) {
-          var blob = this.response;
-          saveAs(blob, "CV_Edson_Rodrigo_PT_BR");
-      }
+  xhr.open("GET", "CV_Edson_Rodrigo_PT_BR.pdf.pdf", true);
+  xhr.responseType = "blob";
+  xhr.onload = function () {
+    if (this.status === 200) {
+      var blob = this.response;
+      saveAs(blob, "CV_Edson_Rodrigo_PT_BR");
+    }
   };
   xhr.send();
-};
-
+}
 
 function copyContacts(text) {
-  const el = document.createElement('textarea');
+  const el = document.createElement("textarea");
   el.value = text;
   document.body.appendChild(el);
   el.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(el);
-  alert('Copiado: ' + text);
-};
+  alert("Copiado: " + text);
+}
 
-
-const slidePortfolios = document.querySelectorAll('.slide-portfolio');
-let currentIndexImgagem = 1;
+let currentIndexImgagem = 0;
 let intervalIdImg;
-let lastHoveredElement;
 
 function showNextImageCardPortfolio() {
-  if (lastHoveredElement) {
-    const elementId = lastHoveredElement.id;
+  const slideImgPortfolioElement =
+    document.querySelectorAll(".slide-portfolio");
 
-    const slideImgPortfolioElement = document.getElementById(elementId);
+  const slideImg =
+    slideImgPortfolioElement[
+      Math.floor(Math.random() * slideImgPortfolioElement.length)
+    ];
 
-    if (slideImgPortfolioElement) {
-      const slideImgPortfolioChildren = slideImgPortfolioElement.querySelectorAll('.slide-img-portfolio');
+  const slideImgPortfolioChildren = slideImg.querySelectorAll(
+    ".slide-img-portfolio"
+  );
+  slideImgPortfolioChildren[currentIndexImgagem].style.opacity = 0;
+  currentIndexImgagem =
+    (currentIndexImgagem + 1) % slideImgPortfolioChildren.length;
+  slideImgPortfolioChildren[currentIndexImgagem].style.opacity = 1;
 
-      slideImgPortfolioChildren[currentIndexImgagem].style.opacity = 0;
-      currentIndexImgagem = (currentIndexImgagem + 1) % slideImgPortfolioChildren.length;
-      slideImgPortfolioChildren[currentIndexImgagem].style.opacity = 1;
-    } 
-    }
-}
-
-function activateShowNextImageCardPortfolio(event) {
-  lastHoveredElement = event.currentTarget;
-  showNextImageCardPortfolio(); 
-  if (!intervalIdImg) {
-    intervalIdImg = setInterval(showNextImageCardPortfolio, 3000);
-
+  if (currentIndexImgagem === slideImgPortfolioChildren.length - 1) {
+    activateShowNextImageCardPortfolio();
   }
 }
-function deactivateShowNextImageCardPortfolio() {
-  clearInterval(intervalIdImg);
-  intervalIdImg = null;
-  currentIndexImgagem = 0;
-  lastHoveredElement = null;
+
+function activateShowNextImageCardPortfolio() {
+  if (!intervalIdImg) {
+    intervalIdImg = setInterval(showNextImageCardPortfolio, 3000);
+  }
 }
-slidePortfolios.forEach(portfolio => {
-  portfolio.addEventListener('mouseenter', activateShowNextImageCardPortfolio);
-  portfolio.addEventListener('mouseleave', deactivateShowNextImageCardPortfolio);
-});
-
-
+activateShowNextImageCardPortfolio();
 
 var modalPreview = document.getElementById("modal-portfolio-preview");
 var span = document.getElementsByClassName("close")[0];
 var btnPreview = document.querySelectorAll(".button-card-port-btn-preview");
 
 btnPreview.forEach(function (button) {
-  button.addEventListener('click', showModalPreviewActive);
+  button.addEventListener("click", showModalPreviewActive);
 });
 
 function showModalPreviewActive(event) {
@@ -263,4 +250,3 @@ window.onclick = function (event) {
     modalPreview.style.display = "none";
   }
 };
-
